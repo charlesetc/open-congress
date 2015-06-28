fb5d72441b3c970eb736518a23da1c6b245d5678
Christopher Walker <cnwalker@uchicago.edu>
2015-06-28 00:30:52 -0500

    Stuff is beginning to work

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -1,18 +1,52 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus',
-	function($scope, Authentication, Menus) {
+angular.module('core').controller('HeaderController', ['$scope', 'Authentication', '$location', '$mdSidenav', '$http', '$mdDialog',
+	function($scope, Authentication, $location, $mdSidenav, $http, $mdDialog) {
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
-		$scope.menu = Menus.getMenu('topbar');
+		//$scope.menu = Menus.getMenu('topbar');
 
-		$scope.toggleCollapsibleMenu = function() {
-			$scope.isCollapsed = !$scope.isCollapsed;
-		};
+
+        $scope.goHome = function() {
+            $location.path('/');
+        };
+
+        $scope.editProfile = function() {
+            $mdSidenav('right').toggle();
+            $location.path('/settings/profile');
+        };
+
+        $scope.updatePword = function() {
+            $mdSidenav('right').toggle();
+            $location.path('/settings/password');
+        };
+
+        $scope.toggleRight = function() {
+            $mdSidenav('right').toggle();
+        };
+
+        $scope.signout = function() {
+            $mdSidenav('right').close();
+
+            $http.get('/auth/signout').success(function(response) {
+                $scope.authentication.user =null;
+                $location.path('/');
+
+            }).error(function(response) {
+                $scope.error = response.message;
+            });
+        };
+
+        $scope.toggleCollapsibleMenu = function() {
+            $scope.isCollapsed = !$scope.isCollapsed;
+        };
+
 
 		// Collapsing the menu after navigation
 		$scope.$on('$stateChangeSuccess', function() {
+			$scope.isSignin = $location.path() === '/signin';
 			$scope.isCollapsed = false;
+
 		});
 	}
 ]);
