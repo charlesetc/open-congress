97e7158e5c2fd7b33f1ddfc5e7907045902c96ea
Christopher Walker <cnwalker@uchicago.edu>
2015-06-27 22:51:20 -0500

    Added material design properly

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'open-congress';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate', 'ngMaterial', 'ngTouch',  'ngSanitize',  'ui.router', 'ui.utils'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
