599d90d0dbb8233c8889886785057d6f6c6845de
Charles Chamberlain <cchamberlain@uchicago.edu>
2015-06-27 21:35:55 -0500

    Senators now have profiles

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -1,4 +1,5 @@
 'use strict';
+var congress = require("../../python_congress_finder/node-congress.js")
 
 /**
  * Module dependencies.
@@ -9,3 +10,40 @@ exports.index = function(req, res) {
 		request: req
 	});
 };
+
+exports.official_profile = function(req, res) {
+	var id = req.params.id;
+	congress.finder("getRepByID", id, function (result) {
+		console.log(JSON.stringify(result));
+		var official = result.results[0];
+		var color;
+		if (official.party === "R") {
+			color = "red";
+		} else if (official.party === "D") {
+			color = "blue";
+		} else {
+			color = "gray";
+		}
+		res.render('official_profile', {
+			user: req.user || null,
+			official: official,
+			color: color,
+			request: req,
+			id: id
+		});
+	});
+};
+
+
+// exports.official_profile = function(req, res) {
+//
+// 	var id = req.params.id;
+// 	congress.finder("getRepByID", req.params.id, function (result) {
+// 		var official = result.results[0];
+// 		res.render('official_profile', {
+// 			user: req.user || null,
+// 			official_name: official.first_name + " " + official.last_name,
+// 			request: req
+// 		});
+// 	}
+// )};
